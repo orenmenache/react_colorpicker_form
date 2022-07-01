89634836e812057d874aa7623e443e6d6239ffc6
Oren <oren.menache@gmail.com>
2022-07-01 16:03:47 +0300

    We're succeeding

--- src/components/MainForm.tsx
+++ src/components/MainForm.tsx
@@ -1,6 +1,5 @@
 import FormRadio from "./form/radio";
 import { FormRadioProps } from "./form/types";
-import { SketchPicker } from "react-color";
 
 const logoOrientation: FormRadioProps = {
     name: 'logoOrientation',
@@ -17,8 +16,31 @@ export default function MainForm(){
     return (
         <div className="main-form">
             <h1>MainForm</h1>
-            <FormRadio name={logoOrientation.name} options={logoOrientation.options}/>
-            <SketchPicker/>
+            <div className="generic-row">
+                <div className="user-input">
+                    <label htmlFor="client-name">Client Name</label>
+                    <input type="text" name="client-name" id="client-name" required/>
+                </div>
+            </div>
+            <div className="generic-row">
+                <div className="file-select">
+                    <label htmlFor="file-selector">Logo Path</label>
+                    <input className="file-selector-input" type="file" name="file-selector" id="file-selector" required/>
+                </div>
+            </div>
+            
+            <div className="generic-col">
+                    <FormRadio name={logoOrientation.name} options={logoOrientation.options}/>
+                    <h2>Select the color pallete</h2>
+                        <div className="generic-row">
+                        <label className="color-picker-label" htmlFor="colorA">Color A:</label>
+                        <input className="color-picker-input" type="color" name="colorA" id="colorA"></input>
+                        <label className="color-picker-label" htmlFor="colorB">Color B:</label>
+                        <input className="color-picker-input" type="color" name="colorB" id="colorB"></input>
+                    </div>
+            </div>
+            
+            
         </div>
         
     );
